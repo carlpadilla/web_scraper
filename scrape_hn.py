b18eca148ce23fd14744f2ed37905d78d48d0a00
Carl Padilla <carlpadilla@protonmail.com>
2020-10-11 17:30:23 -0400

    refactored to include first 2 pages of hacker news

--- scrape_hn.py
+++ scrape_hn.py
@@ -2,30 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
-res = requests.get('https://news.ycombinator.com/')
+urls = ['https://news.ycombinator.com/news', 'https://news.ycombinator.com/news?p=2']  # get the first 2 pages
+num_page = 0  # counter for the page you are currently in
 
-soup = BeautifulSoup(res.text, 'html.parser')
-
-links = soup.select('.storylink')
-subtext = soup.select('.subtext')
-
-
-def sort_stories_by_points(hnlist):
-    return sorted(hnlist, key=lambda k:k['votes'], reverse=True)
-
-
-def create_custom_hn(links, subtext):
-    hn = []
-    for idx, item in enumerate(links):
-        title = links[idx].getText()
-        href = links[idx].get('href', None)
-        vote = subtext[idx].select('.score')
-        if len(vote):
-            points = int(vote[0].getText().replace(' points', ''))
-            if points > 99:
-                hn.append({'title': title, 'link': href, 'votes': points})
-
-    return sort_stories_by_points(hn)
+for url in urls:  # loops through or list of urls and repeats the process :)
+    num_page += 1
+    res = requests.get(url)
+    BeautifulSoup(res.text, 'html.parser')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    links = soup.select('.storylink')
+    subtext = soup.select('.subtext')
 
 
-pprint.pprint(create_custom_hn(links, subtext))
+    def sort_stories_by_votes(hnlist):
+        return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+    def create_custom_hn(link, subtext):
+        hn = []
+        for idx, item in enumerate(link):
+            title = item.getText()
+            href = item.get('href', None)
+            vote = subtext[idx].select('.score')
+            if len(vote):
+                points = int(vote[0].getText().replace(' points', ''))
+                if points > 99:
+                    hn.append({'title': title, 'link': href, 'votes': points})
+        return sort_stories_by_votes(hn)
+
+
+    print(f' ***Currently on page num: {num_page} ***')  # print num_page
+    pprint.pprint(create_custom_hn(links, subtext))
